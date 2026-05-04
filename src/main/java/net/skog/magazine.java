98eec8ff204054b0ef43ca12a9531d2f83b370a2
SkogisAB <filip.skoglund07@outlook.com>
2026-05-04 20:47:37 +0200

    Magazine class

---
+++ src/main/java/net/skog/magazine.java
@@ -0,0 +1,26 @@
+package net.skog;
+
+public class magazine extends Publication {
+    private Integer issueNumber;
+    private String category;
+    private Integer publishedYear;
+
+    public magazine(String id, String title, boolean isAvailable, Integer issueNumber, String category,
+            Integer publishedYear) {
+        super(id, title, isAvailable);
+        this.issueNumber = issueNumber;
+        this.category = category;
+        this.publishedYear = publishedYear;
+    }
+
+    public Integer getIssueNumber() {
+        return issueNumber;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public Integer getPublishedYear() {
+        return publishedYear;
+    }
+
+}
